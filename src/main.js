e9eef0152710159e90b499baa51c0cda5ed50a4f
Egor Krotov <ego_rk@mail.ru>
2021-06-16 02:20:22 +0300

    socket1

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import '@/assets/scss/style.scss'
 
 const baseServerUrl=process.env.NODE_ENV=='development'? 'https://localhost:44329' :'https://diplom-server.azurewebsites.net' ;
 const baseClientUrl= process.env.NODE_ENV=='development'? 'http://localhost:8080':'https://diplom.westeurope.cloudapp.azure.com';
-const webrtcServer =process.env.NODE_ENV=='development'? 'ws://localhost:3000': 'wss://bscure-hollows-66091.herokuapp.com';
+const webrtcServer =process.env.NODE_ENV=='development'? 'http://localhost:3000': 'https://bscure-hollows-66091.herokuapp.com';
 
 export {
   baseServerUrl,
